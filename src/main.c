88486c44d5a0b4b45411dd242ced977b4e1dd1f4
Sawyerf <sawyer.flink@protonmail.ch>
2020-10-30 17:41:40 +0100

    Add: First working ping

--- src/main.c
+++ src/main.c
@@ -10,10 +10,10 @@
 typedef struct addrinfo t_addrinfo;
 typedef struct icmphdr t_icmphdr;
 
-void atos(char *hostname, t_addrinfo *ai)
+char *atos(t_addrinfo *ai)
 {
 	struct addrinfo * _res;
-	char _address[INET6_ADDRSTRLEN];
+	char address[INET6_ADDRSTRLEN];
 
 	for(_res = ai; _res != NULL; _res = _res->ai_next)
 	{
@@ -21,67 +21,87 @@ void atos(char *hostname, t_addrinfo *ai)
 		{
 			if (NULL == inet_ntop(AF_INET,
 				&((struct sockaddr_in *)_res->ai_addr)->sin_addr,
-					_address,sizeof(_address) ))
+					address,sizeof(address) ))
 			{
 				perror("inet_ntop");
+				return NULL;
 			}
-			ft_printf("PING %s(??? (%s)) ?? data bytes\n", hostname, _address);
 		}
 	}
+	return ft_strdup(address);
 }
 
 void fill_icmp(t_icmphdr *icmp)
 {
 	memset(icmp, 0, sizeof(t_icmphdr));
 	icmp->type = 8;
+	icmp->un.echo.id = 1234;
 }
 
-int main(int arg, char **argv)
+t_addrinfo *get_addr(char *host)
 {
-	(void)arg;
-	int sock;
 	t_addrinfo hints;
-	t_addrinfo *ai;
 	t_addrinfo *result;
-
-    unsigned char data[2048];
-	t_icmphdr icmp;
-    t_icmphdr ricmp;
-	int s; 
-
-	fill_icmp(&icmp);
-    memcpy(data, &icmp, sizeof(t_icmphdr));
+	int s;
 
 	memset(&hints, 0, sizeof (struct addrinfo));
 	hints.ai_family   = AF_INET;
 	hints.ai_socktype = SOCK_RAW;
 	hints.ai_protocol = IPPROTO_ICMP;
 
-	if ((s = getaddrinfo (argv[1], NULL, &hints, &result)) != 0)
+	if ((s = getaddrinfo (host, NULL, &hints, &result)) != 0)
 	{
 		ft_printf ("getaddrinfo: %s\n", gai_strerror (s));
 		exit (EXIT_FAILURE);
+		return NULL;
 	}
-	ai = result;
-	atos(argv[1], ai);
+	result->ai_socktype = SOCK_DGRAM;
+	return result;
+}
 
+int main(int arg, char **argv)
+{
+	t_addrinfo *ai;
+    t_icmphdr icmp_hdr;
+    t_icmphdr rcv_hdr;
+    int sock;
+    int rc;
+	char *address;
+
+	ai = get_addr(argv[1]);
+	address = atos(ai);
+	ft_printf("PING %s(%s) ?? data bytes\n", argv[1], address);
 	sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
-	if (sock < 0)
-		ft_printf("fail: %d\n", sock);
-	s = sendto(sock, data, sizeof(t_icmphdr), 0, ai->ai_addr, ai->ai_addrlen);
-	ft_printf("sendto: %d\n", sock);
-
-	socklen_t slen = 0;
-	int rc = 0;
-	
-	ft_printf("ici\n");
-    rc = recvfrom(sock, &ricmp, sizeof(t_icmphdr), 0, NULL, &slen);
-    if (rc <= 0) {
-        perror("recvfrom");
-        return 1;
-    } else if (rc < sizeof(ricmp)) {
-        ft_printf("Error, got short ICMP packet, %d bytes\n", rc);
+    if (sock < 0) {
+        perror("socket");
         return 1;
     }
-	ft_printf("recv: %d\n", rc);
+	
+    memset(&icmp_hdr, 0, sizeof icmp_hdr);
+    icmp_hdr.type = 8;
+    icmp_hdr.un.echo.sequence = 0;
+    icmp_hdr.un.echo.id = 1234;
+
+    while (1){
+
+        icmp_hdr.un.echo.sequence++;
+		rc = sendto(sock, &icmp_hdr, sizeof(icmp_hdr), 0, ai->ai_addr, ai->ai_addrlen);
+        if (rc <= 0) {
+            perror("Sendto");
+            break;
+        }
+
+        rc = recvfrom(sock, &rcv_hdr, sizeof(rcv_hdr), 0, ai->ai_addr, &ai->ai_addrlen);
+        if (rc <= 0) {
+            perror("recvfrom");
+            break;
+        } else if (rc < sizeof(rcv_hdr)) {
+            ft_printf("Error, got short ICMP packet, %d bytes\n", rc);
+            break;
+        }
+                            
+		ft_printf("?? bytes from %s: icmp_seq=%d ttl=?? time=?? ms type=%d\n", address, icmp_hdr.un.echo.sequence, rcv_hdr.type);
+		if (icmp_hdr.un.echo.sequence == 3)
+			break;
+    }
 }
